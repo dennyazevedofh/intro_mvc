5a99748a151a341aa8df9da05cfa839cfac65001
DennyAzevedo <denny_af@yahoo.com.br>
2025-03-24 15:42:14 -0300

    Criando a View

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 const alunos = [
 	{
-	  _id: 0,
 	  nome: "chico melato",
 	  notas: {
 		backend_1: [7, 8.5, 9, 6.5],
@@ -10,7 +9,6 @@ const alunos = [
 	  },
 	},
 	{
-	  _id: 1,
 	  nome: "talita lima",
 	  notas: {
 		backend_1: [4, 4, 4, 4],
@@ -20,7 +18,6 @@ const alunos = [
 	  },
 	},
 	{
-	  _id: 2,
 	  nome: "Fulano da Silva",
 	  notas: {
 		backend_1: [4, 4, 4, 4],
@@ -37,52 +34,5 @@ alunos.forEach(aluno => {
 	alunoService.add(new AlunoModel(aluno))
 })
 
-// Inserir no thead da tabela a lista de matérias
-const htmlHeader = document.createElement('tr')
-htmlHeader.innerHTML = '<td>Nome</td>'
-
-const htmlHeaderMaterias = Object.keys(alunos[0].notas).map(materia => {
-	return `<td>${materia}</td>`
-}).join('')
-
-htmlHeader.innerHTML += htmlHeaderMaterias
-document.querySelector('[data-table-alunos] thead').appendChild(htmlHeader)
-
-// Inserir no tbody da tabela a lista de alunos e suas médias
-function render() {
-	document.querySelector('[data-table-alunos] tbody').innerHTML = ''
-	alunos.forEach(aluno => {
-		let htmlRow = document.createElement('tr')
-		htmlRow.innerHTML = `<td>${aluno.nome}</td>`
-		let htmlRowMaterias = Object.keys(aluno.media).map(materia => {
-			return `<td>${aluno.media[materia]}</td>`
-		}).join('')
-		htmlRow.innerHTML += htmlRowMaterias
-		document.querySelector('[data-table-alunos] tbody').appendChild(htmlRow)
-	})
-}
-render()
-
-// Inserir aluno
-document.querySelector('form').addEventListener('submit', function(event) {
-	event.preventDefault()
-	const nome = document.getElementById('first_name').value
-	console.log(nome)
-	const newAluno = {
-		_id: 0,
-		nome,
-		notas: {
-			backend_1: [6, 6, 6, 6],
-			frontend_2: [7, 7, 7, 7],
-			bancodados: [6, 6, 6, 6],
-			ferramentas: [7,7,7,7],
-		},
-	}
-	newAluno.media = {}
-	for (let materia in newAluno.notas) {
-		newAluno.media[materia] = average(...newAluno.notas[materia])
-	}
-	alunos.push(newAluno)
-	render()
-})
-
+const alunoView = new AlunoView(document.querySelector('[data-table-alunos]'))
+const alunoController = new AlunoController(alunoService, alunoView)
