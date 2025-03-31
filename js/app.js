5d3b4b818b10e9509c2af190823a48a6400bc070
DennyAzevedo <denny_af@yahoo.com.br>
2025-03-31 15:14:24 -0300

    Usando LocalStorage

--- js/app.js
+++ js/app.js
@@ -1,38 +1,4 @@
-const alunos = [
-	{
-	  nome: "chico melato",
-	  notas: {
-		backend_1: [7, 8.5, 9, 6.5],
-		frontend_2: [6, 8, 5, 7],
-		bancodados: [7, 7, 6.5, 8],
-		ferramentas: [3, 3, 3, 3],
-	  },
-	},
-	{
-	  nome: "talita lima",
-	  notas: {
-		backend_1: [4, 4, 4, 4],
-		frontend_2: [4, 4, 5, 5],
-		bancodados: [5, 5, 6, 6],
-		ferramentas: [7, 7, 8, 9],
-	  },
-	},
-	{
-	  nome: "Fulano da Silva",
-	  notas: {
-		backend_1: [4, 4, 4, 4],
-		frontend_2: [4, 4, 5, 5],
-		bancodados: [5, 5, 6, 6],
-		ferramentas: [7, 7, 8, 9],
-	  },
-	},
-];
-  
 const alunoService = new AlunoService()
-
-alunos.forEach(aluno => {
-	alunoService.add(new AlunoModel(aluno))
-})
 
 const alunoView = new AlunoView(document.querySelector('[data-table-alunos]'))
 const alunoController = new AlunoController(alunoService, alunoView)
